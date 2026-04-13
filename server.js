637b02fcdbc0e686fd3652003df87e8fda0891c5
robotto-ping <49987778+robotto-ping@users.noreply.github.com>
2026-04-13 08:10:48 -0600

    fetch fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
+
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
