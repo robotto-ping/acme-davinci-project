8246407d4d70c2ee9cf271a92582fc35a335d5b9
robotto-ping <49987778+robotto-ping@users.noreply.github.com>
2026-04-15 18:18:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // Environment Variables needed in Railway:
 // DV_COMPANY_ID, DV_API_KEY, DV_REGION (e.g., 'com', 'eu', 'asia')
-const REGION = process.env.DV_REGION || 'eu'; 
+const REGION = process.env.DV_REGION || 'eu';
 const API_ROOT = `https://auth.pingone.${REGION}`;
 const ORCHESTRATE_BASE_URL = `https://orchestrate-api.pingone.${REGION}/v1`;
 
@@ -65,17 +65,49 @@ app.post('/dvtoken', async (req, res) => {
     }
 });
 
-app.post('/auth/login', (req, res) => {
-    const { sessionToken } = req.body;
+app.post('/auth/login', async (req, res) => {
+    try {
+        const { sessionToken } = req.body;
+        if (!sessionToken) return res.status(400).send("Missing token");
+        const policyId = "cb71ac46ae7129de917e631204285f95";
+        const companyId = process.env.DV_COMPANY_ID;
+        const apiKey = process.env.DV_API_KEY;
 
-    if (!sessionToken) return res.status(400).send("Missing token");
+        // Construct the body for the SDK Token request
+        let body = { policyId: policyId };
+        body.global = { sessionToken: sessionToken };
 
-    // Store tokens in the server-side session
-    // The browser never sees these!
-   // req.session.dvSessionToken = sessionToken;
+        const response = await fetch(`${ORCHESTRATE_BASE_URL}/company/${companyId}/sdktoken`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-SK-API-KEY': apiKey
+            },
+            body: JSON.stringify(body)
+        });
 
+        const data = await response.json();
+        const sdkToken = data.access_token;
 
-    res.json({ message: "Session established" });
+        body = {}; 
+
+        response = await fetch(`${ORCHESTRATE_BASE_URL}/company/${companyId}/policy/${policyId}/start`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer: ' + sdkToken
+            },
+            body: JSON.stringify(body)
+        });
+
+        console.info(response);
+
+        res.json({ message: "Session established" });
+
+    } catch (error) {
+        console.error("BFF Error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 app.listen(PORT, () => console.log(`Acme BFF live on port ${PORT}`));
